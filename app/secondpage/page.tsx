8df87db6fca51915c01524513fde094718bee63e
BayerTu <arthurdias0013@gmail.com>
2026-02-13 19:57:06 -0300

    text

--- app/secondpage/page.tsx
+++ app/secondpage/page.tsx
@@ -2,13 +2,17 @@
 import { Confetti, ConfettiRef } from "@/components/ui/confetti"
 import { useRef } from "react"
 import Image from "next/image"
-
+import { MorphingText } from "@/components/ui/morphing-text"
+const texts = [
+  "EU TE AMO",
+  "FELIZ DIA DOS NAMORADOS",
+]
 
 
 export default function SecondPage() {
   const confettiRef = useRef<ConfettiRef>(null)
   return (
-    <main className="flex min-h-screen items-center justify-center bg-pink-300 bg-black flex-col">
+    <main className="flex min-h-screen items-center justify-center bg-pink-300 gap-2 flex-col">
       <Confetti
         ref={confettiRef}
         className="absolute top-0 left-0 z-0 size-full"
@@ -16,14 +20,19 @@ export default function SecondPage() {
           confettiRef.current?.fire({})
         }}
       />
-
-      <Image
-        src="/jappones.jpeg"
-        width={600}
-        height={400}
-        className="rounded-3xl" alt={""}
-      />
-
+      <section className="flex flex-col gap-10">
+        <div>
+          <Image
+            src="/jappones.jpeg"
+            width={500}
+            height={300}
+            className="rounded-3xl " alt={""}
+          />
+        </div>
+        <div className="">
+          <MorphingText texts={texts} />
+        </div>
+      </section>
     </main>
   )
 }
