acc7ec42ab45d4ed2d00cd712d358b180dd4c2d0
BayerTu <arthurdias0013@gmail.com>
2026-02-13 19:47:47 -0300

    image

--- app/secondpage/page.tsx
+++ app/secondpage/page.tsx
@@ -1,11 +1,29 @@
-
+'use client'
+import { Confetti, ConfettiRef } from "@/components/ui/confetti"
+import { useRef } from "react"
+import Image from "next/image"
 
 
 
 export default function SecondPage() {
+  const confettiRef = useRef<ConfettiRef>(null)
   return (
-    <main className="flex min-h-screen items-center justify-center bg-pink-300">
-      <h1>Segunda página</h1>
+    <main className="flex min-h-screen items-center justify-center bg-pink-300 bg-black flex-col">
+      <Confetti
+        ref={confettiRef}
+        className="absolute top-0 left-0 z-0 size-full"
+        onMouseEnter={() => {
+          confettiRef.current?.fire({})
+        }}
+      />
+
+      <Image
+        src="/jappones.jpeg"
+        width={600}
+        height={400}
+        className="rounded-3xl" alt={""}
+      />
+
     </main>
   )
 }
